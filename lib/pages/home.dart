1606efdf5dae21210bf3dc6c5a8136faf0afc8c5
KevinNizet <kevin.nizet@gmail.com>
2023-07-16 15:15:11 +0200

    ajout de la fonction onTap() pour les boutons de la navBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,37 +7,51 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Breakfast',
-          style: TextStyle(
-              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: Container(
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+            color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {
+          
+        },
+        child: Container(
           margin: const EdgeInsets.all(10),
           alignment: Alignment.center,
-          child:
-              SvgPicture.asset('assets/icons/arrow.svg', height: 15, width: 15),
           decoration: BoxDecoration(
               color: const Color(0xffF7F8F8),
               borderRadius: BorderRadius.circular(10)),
+          child:
+              SvgPicture.asset('assets/icons/arrow.svg', height: 15, width: 15),
         ),
-        actions: [
-          Container(
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {
+
+          },
+          child: Container(
             margin: const EdgeInsets.all(10),
             alignment: Alignment.center,
             width: 37,
-            child:
-                SvgPicture.asset('assets/icons/dot.svg', height: 20, width: 20),
             decoration: BoxDecoration(
                 color: const Color(0xffF7F8F8),
                 borderRadius: BorderRadius.circular(10)),
+            child:
+                SvgPicture.asset('assets/icons/dot.svg', height: 20, width: 20),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
